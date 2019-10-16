9aee9356a7bb319ae6ec19466b8fa3860614586b
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-10-16 15:57:13 -0400

    added onChange listeners

--- team-builder/src/Forms/Form.js
+++ team-builder/src/Forms/Form.js
@@ -2,32 +2,66 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 export const Form = props => {
-  const [name, setName] = useState("");
+  const [fName, setFName] = useState("");
   const [lName, setLName] = useState("");
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
 
   return (
-    <form>
-      <labal htmlFor="fNameInput">First Name</labal>
-      <input id="fNameInput" name="fName" type="text" />
+    <div>
+      <h3>
+        {fName || "unknown"} {lName || "unknown"} is a {role} with an email of{" "}
+        {email}.
+      </h3>
+      <form>
+        <labal htmlFor="fNameInput">First Name</labal>
+        <input
+          onChange={e => {
+            setFName(e.target.value);
+          }}
+          id="fNameInput"
+          name="fName"
+          type="text"
+        />
 
-      <labal htmlFor="lNameInput">Last Name</labal>
-      <input id="lNameInput" name="lName" type="text" />
+        <labal htmlFor="lNameInput">Last Name</labal>
+        <input
+          onChange={e => {
+            setLName(e.target.value);
+          }}
+          id="lNameInput"
+          name="lName"
+          type="text"
+        />
 
-      <labal htmlFor="emailInput">Role</labal>
-      <input id="lNameInput" name="lName" type="email" />
+        <labal htmlFor="emailInput">Email</labal>
+        <input
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+          id="lNameInput"
+          name="lName"
+          type="email"
+        />
 
-      <select id="roleSelect" name="role">
-        <option value="1">Front-end Engineer</option>
-        <option value="2">Back-end Engineer</option>
-        <option value="3">UI Designer</option>
-        <option value="4">IOS Developer</option>
-        <option value="5">Android Developer</option>
-        <option value="6">Data Scientist</option>
-      </select>
+        <labal htmlFor="roleInput">Role</labal>
+        <select
+          onChange={e => {
+            setRole(e.target.value);
+          }}
+          id="roleSelect"
+          name="role"
+        >
+          <option value="Front-end Engineer">Front-end Engineer</option>
+          <option value="Back-end Engineer">Back-end Engineer</option>
+          <option value="UI Designer">UI Designer</option>
+          <option value="IOS Developer">IOS Developer</option>
+          <option value="Android Developer">Android Developer</option>
+          <option value="Data Scientist">Data Scientist</option>
+        </select>
 
-      <input type="submit" />
-    </form>
+        <input type="submit" />
+      </form>
+    </div>
   );
 };
